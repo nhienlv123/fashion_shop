0ec9a0053e622aaa17277bbdda9d978a759b16d2
nhienlv123 <nhien.lv@anlab.biz>
2022-04-07 08:45:11 +0700

    upload

---
+++ resources/views/client/master.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>Karl - Fashion Ecommerce Template | Home</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="../../assets/img/core-img/favicon.ico">
+
+    <!-- Core Style CSS -->
+    <link rel="stylesheet" href="../../assets/css/core-style.css">
+    <link rel="stylesheet" href="../../assets/style.css">
+
+    <!-- Responsive CSS -->
+    <link href="../../assets/css/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="catagories-side-menu">
+        <!-- Close Icon -->
+        <div id="sideMenuClose">
+            <i class="ti-close"></i>
+        </div>
+        <!--  Side Nav  -->
+        <div class="nav-side-menu">
+            <div class="menu-list">
+                <h6>Categories</h6>
+                <ul id="menu-content" class="menu-content collapse out">
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#women" class="collapsed active">
+                        <a href="#">Woman wear <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="women">
+                            <li><a href="#">Midi Dresses</a></li>
+                            <li><a href="#">Maxi Dresses</a></li>
+                            <li><a href="#">Prom Dresses</a></li>
+                            <li><a href="#">Little Black Dresses</a></li>
+                            <li><a href="#">Mini Dresses</a></li>
+                        </ul>
+                    </li>
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#man" class="collapsed">
+                        <a href="#">Man Wear <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="man">
+                            <li><a href="#">Man Dresses</a></li>
+                            <li><a href="#">Man Black Dresses</a></li>
+                            <li><a href="#">Man Mini Dresses</a></li>
+                        </ul>
+                    </li>
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#kids" class="collapsed">
+                        <a href="#">Children <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="kids">
+                            <li><a href="#">Children Dresses</a></li>
+                            <li><a href="#">Mini Dresses</a></li>
+                        </ul>
+                    </li>
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#bags" class="collapsed">
+                        <a href="#">Bags &amp; Purses <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="bags">
+                            <li><a href="#">Bags</a></li>
+                            <li><a href="#">Purses</a></li>
+                        </ul>
+                    </li>
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#eyewear" class="collapsed">
+                        <a href="#">Eyewear <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="eyewear">
+                            <li><a href="#">Eyewear Style 1</a></li>
+                            <li><a href="#">Eyewear Style 2</a></li>
+                            <li><a href="#">Eyewear Style 3</a></li>
+                        </ul>
+                    </li>
+                    <!-- Single Item -->
+                    <li data-toggle="collapse" data-target="#footwear" class="collapsed">
+                        <a href="#">Footwear <span class="arrow"></span></a>
+                        <ul class="sub-menu collapse" id="footwear">
+                            <li><a href="#">Footwear 1</a></li>
+                            <li><a href="#">Footwear 2</a></li>
+                            <li><a href="#">Footwear 3</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div id="wrapper">
+    @include("client.header")
+
+    @yield("content")
+    
+    @include("client.footer")
+    </div>
+    <!-- /.wrapper end -->
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="../../assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="../../assets/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="../../assets/js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="../../assets/js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="../../assets/js/active.js"></script>
+
+</body>
+
+</html>
